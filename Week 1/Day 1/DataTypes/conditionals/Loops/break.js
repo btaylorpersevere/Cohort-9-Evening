8410ceab0dc01a3b676a3c05e2dc30eeed58a7d3
btaylorpersevere <btaylor@perseverenow.org>
2024-11-06 16:08:02 -0600

    updated notes on loops

---
+++ Week 1/Day 1/DataTypes/conditionals/Loops/break.js
@@ -0,0 +1,11 @@
+break
+Break is used to interrupt a loop.
+
+for(let i = 0; i <= 5; i++){
+  if(i == 3){
+    break
+  }
+  console.log(i)
+}
+
+// 0 1 2
---
+++ Week 1/Day 1/DataTypes/conditionals/Loops/continue.js
@@ -0,0 +1,11 @@
+continue
+We use the keyword continue to skip a certain iterations.
+
+for(let i = 0; i <= 5; i++){
+  if(i == 3){
+    continue
+  }
+  console.log(i)
+}
+
+// 0 1 2 4 5
