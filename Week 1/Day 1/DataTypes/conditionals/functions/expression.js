5d3088b867cbd4cf366a0376617f79b54a6ea36a
btaylorpersevere <btaylor@perseverenow.org>
2024-11-14 14:17:59 -0600

    Added additional notes on functions

---
+++ Week 1/Day 1/DataTypes/conditionals/functions/expression.js
@@ -0,0 +1,9 @@
+// Expression Function
+// Expression functions are anonymous functions. After we create a function without a name and we assign it to a variable. To return a value from the function we should call the variable. Look at the example below.
+
+// Function expression
+const square = function(n) {
+  return n * n
+}
+
+console.log(square(2)) // -> 4
---
+++ Week 1/Day 1/DataTypes/conditionals/functions/selfinvoking.js
@@ -0,0 +1,12 @@
+// Self Invoking Functions
+// Self invoking functions are anonymous functions which do not need to be called to return a value.
+
+(function(n) {
+  console.log(n * n)
+})(2) // 4, but instead of just printing if we want to return and store the data, we do as shown below
+
+let squaredNum = (function(n) {
+  return n * n
+})(10)
+
+console.log(squaredNum)
