17fbc9fac914166ad0a9225c49027cb57df0053d
btaylorpersevere <btaylor@perseverenow.org>
2024-12-18 20:05:47 -0600

    updated homework for classes

---
+++ JS Classes/classex.js
@@ -0,0 +1,5 @@
+// Exercises Level 1
+// Create an Animal class. The class will have name, age, color, legs properties and create different methods
+// Create a Dog and Cat child class from the Animal Class.
+// Exercises Level 2
+// Override the method you create in Animal class
